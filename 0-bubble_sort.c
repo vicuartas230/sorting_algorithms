96d7954b612c20c6f6950124f51a5be7850cbc8f
victor <vicuartas230@gmail.com>
2021-06-11 12:18:27 -0500

    Uploading first files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This function
+ * @array: The array to sort.
+ * @size: The length of the array to sort.
+ * Return: Nothing.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    unsigned int i = 0, flag = 1;
+    int aux = 0;
+
+    while (flag)
+    {
+        flag = 0;
+        i = 0;
+        while (i < size - 1)
+        {
+            if (array[i + 1] < array[i])
+            {
+                aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+                flag = 1;
+            }
+            i++;
+        }
+    }
+    
+}

61182c8d70e212c613d56a0ad09d8cfb68d198fc
victor <vicuartas230@gmail.com>
2021-06-11 12:21:05 -0500

    Fixing style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i = 0, flag = 1;
-    int aux = 0;
+	unsigned int i = 0, flag = 1;
+	int aux = 0;
 
-    while (flag)
-    {
-        flag = 0;
-        i = 0;
-        while (i < size - 1)
-        {
-            if (array[i + 1] < array[i])
-            {
-                aux = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = aux;
-                flag = 1;
-            }
-            i++;
-        }
-    }
-    
+	while (flag)
+	{
+		flag = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (array[i + 1] < array[i])
+			{
+				aux = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux;
+				flag = 1;
+			}
+			i++;
+		}
+	}
 }

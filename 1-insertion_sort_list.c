27c37018829bf1103cd33743b1683e9c5d65189f
victor <vicuartas230@gmail.com>
2021-06-12 15:02:38 -0500

    Adding functions to the code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,109 +9,151 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tmp = *list, *tmp2 = NULL, *hold = NULL;
-    int flag = 0;
+	listint_t *tmp = *list, *hold = NULL;
+	int flag = 0;
 
-    while (tmp->next)
-    {
-        if (tmp->next->n < tmp->n && tmp == *list)
-        {
-            tmp2 = tmp->next;
-            *list = tmp2;
-            tmp->next = tmp2->next;
-            tmp->prev = tmp2;
-            tmp2->next->prev = tmp;
-            tmp2->next = tmp;
-            tmp2->prev = NULL;
-            print_list(*list);
-        }
-        if (tmp->next->n < tmp->n && tmp->next->next)
-        {
-            tmp2 = tmp->next;
-            tmp->next = tmp2->next;
-            tmp->next->prev = tmp;
-            tmp2->next = tmp;
-            tmp2->prev = tmp->prev;
-            tmp->prev = tmp2;
-            tmp2->prev->next = tmp2;
-            tmp = tmp->prev;
-            print_list(*list);
-            if (tmp && tmp->n < tmp->prev->n)
-                hold = tmp->next;
-            while (tmp && tmp->n < tmp->prev->n)
-            {
-                flag = 1;
-                if (tmp->prev->prev)
-                {
-                    tmp = tmp->prev;
-                    tmp->next = tmp2->next;
-                    tmp->next->prev = tmp;
-                    tmp2->next = tmp;
-                    tmp2->prev = tmp->prev;
-                    tmp->prev = tmp2;
-                    tmp2->prev->next = tmp2;
-                    print_list(*list);
-                }
-                else if (tmp->prev)
-                {
-                    tmp->next->prev = *list;
-                    (*list)->next = tmp->next;
-                    (*list)->prev = tmp;
-                    tmp->next = *list;
-                    tmp->prev = NULL;
-                    *list = tmp;
-                    print_list(*list);
-                }
-                tmp = tmp->prev;
-            }
-            if (flag)
-            {
-                tmp = hold->prev;
-                flag = 0;
-            }
-        }
-        if (tmp->next->n < tmp->n && !tmp->next->next)
-        {
-            tmp2 = tmp->next;
-            tmp2->next = tmp;
-            tmp2->prev = tmp->prev;
-            tmp2->prev->next = tmp2;
-            tmp->prev = tmp2;
-            tmp->next = NULL;
-            tmp = tmp->prev;
-            print_list(*list);
-            while (tmp && tmp->n < tmp->prev->n)
-            {
-                flag = 1;
-                if (tmp->prev->prev)
-                {
-                    tmp = tmp->prev;
-                    tmp->next = tmp2->next;
-                    tmp->next->prev = tmp;
-                    tmp2->next = tmp;
-                    tmp2->prev = tmp->prev;
-                    tmp->prev = tmp2;
-                    tmp2->prev->next = tmp2;
-                    print_list(*list);
-                }
-                else if (tmp->prev)
-                {
-                    tmp->next->prev = *list;
-                    (*list)->next = tmp->next;
-                    (*list)->prev = tmp;
-                    tmp->next = *list;
-                    tmp->prev = NULL;
-                    *list = tmp;
-                    print_list(*list);
-                }
-                tmp = tmp->prev;
-            }
-            if (flag)
-            {
-                tmp = hold->prev;
-                flag = 0;
-            }
-        }
-        tmp = tmp->next;
-    }
+	while (tmp->next)
+	{
+		if (tmp->next->n < tmp->n && tmp == *list)
+			swap_start(list, tmp);
+		if (tmp->next->n < tmp->n && tmp->next->next)
+			swap_middle(list, tmp);
+		if (tmp->next->n < tmp->n && !tmp->next->next)
+		{
+			swap_end(list, tmp);
+			tmp = tmp->prev;
+			if (tmp && tmp->n < tmp->prev->n)
+				hold = tmp->next;
+			while (tmp && tmp->n < tmp->prev->n)
+			{
+				flag = 1;
+				if (tmp->prev->prev)
+				{
+					tmp = tmp->prev;
+					swap_middle(list, tmp);
+				}
+				else if (tmp->prev)
+				{
+					swap(list, tmp);
+				}
+				tmp = tmp->prev;
+			}
+			if (flag)
+			{
+				tmp = hold->prev;
+				flag = 0;
+			}
+		}
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * swap_start - This function swaps two nodes at the start of a linked list.
+ * @list: The start of the linked list.
+ * @tmp: The pointer to node to swap.
+ * Return: Nothing
+ */
+
+void swap_start(listint_t **list, listint_t *tmp)
+{
+	listint_t *tmp2 = NULL;
+
+	tmp2 = tmp->next;
+	*list = tmp2;
+	tmp->next = tmp2->next;
+	tmp->prev = tmp2;
+	tmp2->next->prev = tmp;
+	tmp2->next = tmp;
+	tmp2->prev = NULL;
+	print_list(*list);
+}
+
+/**
+ * swap_middle - This function swaps two nodes at the middle of a linked list.
+ * @list: The start of the linked list.
+ * @tmp: The pointer to node to swap.
+ * Return: Nothing
+ */
+
+void swap_middle(listint_t **list, listint_t *tmp)
+{
+	listint_t *tmp2 = tmp->next, *hold = NULL;
+	int flag = 0;
+
+	tmp->next = tmp2->next;
+	tmp->next->prev = tmp;
+	tmp2->next = tmp;
+	tmp2->prev = tmp->prev;
+	tmp->prev = tmp2;
+	tmp2->prev->next = tmp2;
+	print_list(*list);
+	tmp = tmp->prev;
+	if (tmp && tmp->n < tmp->prev->n)
+		hold = tmp->next;
+	while (tmp && tmp->n < tmp->prev->n)
+	{
+		flag = 1;
+		if (tmp->prev->prev)
+		{
+			tmp = tmp->prev;
+			tmp->next = tmp2->next;
+			tmp->next->prev = tmp;
+			tmp2->next = tmp;
+			tmp2->prev = tmp->prev;
+			tmp->prev = tmp2;
+			tmp2->prev->next = tmp2;
+			print_list(*list);
+			/* swap_middle(list, tmp); */
+		}
+		else if (tmp->prev)
+		{
+			swap(list, tmp);
+		}
+		tmp = tmp->prev;
+	}
+	if (flag)
+	{
+		tmp = hold->prev;
+		flag = 0;
+	}
+}
+
+/**
+ * swap_end - This function swaps two nodes at the end of a linked list.
+ * @list: The start of the linked list.
+ * @tmp: The pointer to node to swap.
+ * Return: Nothing
+ */
+
+void swap_end(listint_t **list, listint_t *tmp)
+{
+	listint_t *tmp2 = NULL;
+
+	tmp2 = tmp->next;
+	tmp2->next = tmp;
+	tmp2->prev = tmp->prev;
+	tmp2->prev->next = tmp2;
+	tmp->prev = tmp2;
+	tmp->next = NULL;
+	tmp = tmp->prev;
+	print_list(*list);
+}
+
+/**
+ * swap - This function swaps two nodes with the head.
+ * @list: The start of the linked list.
+ * @tmp: The pointer to node to swap.
+ * Return: Nothing
+ */
+
+void swap(listint_t **list, listint_t *tmp)
+{
+	tmp->next->prev = *list;
+	(*list)->next = tmp->next;
+	(*list)->prev = tmp;
+	tmp->next = *list;
+	tmp->prev = NULL;
+	*list = tmp;
+	print_list(*list);
 }
